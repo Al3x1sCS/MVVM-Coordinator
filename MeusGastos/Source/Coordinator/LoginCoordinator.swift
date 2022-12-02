db794c8a7e902a36bab4a3c7916db0735f14d36f
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-02 15:05:24 -0300

    feat: adding loginCoordinator.

--- MeusGastos/Source/Coordinator/LoginCoordinator.swift
+++ MeusGastos/Source/Coordinator/LoginCoordinator.swift
@@ -5,4 +5,18 @@
 //  Created by user on 02/12/22.
 //
 
-import Foundation
+import UIKit
+
+class LoginCoordinator: Coordinator {
+    let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewController = LoginViewController()
+        self.navigationController.pushViewController(viewController, animated: true)
+    }
+    
+}
