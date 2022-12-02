c8c876dca0a262f7365deb6c058b8c8964487641
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-02 15:02:36 -0300

    feat: cleaning the project.

---
+++ MeusGastos/Source/Coordinator/LoginCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  LoginCoordinator.swift
+//  MeusGastos
+//
+//  Created by user on 02/12/22.
+//
+
+import Foundation
---
+++ MeusGastos/Source/ViewControllers/Login/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  MeusGastos
+//
+//  Created by user on 02/12/22.
+//
+
+import Foundation
