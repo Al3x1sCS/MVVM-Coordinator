7717ff8d5dd34b8a1e06c9bfca16c835b8b606fd
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-02 14:34:10 -0300

    feat: starting project MeusGastos.  Foi criado uma pasta contendo o projeto  Meus Gastos.  - Tambem foi adicionado .gitignor.

---
+++ MeusGastos/MeusGastos/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MeusGastos
+//
+//  Created by user on 01/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
---
+++ MeusGastos/MeusGastos/ViewController2.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  MeusGastos
+//
+//  Created by user on 02/12/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
