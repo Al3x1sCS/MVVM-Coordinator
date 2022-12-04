24ee2d1ed780859452c5e28b17e37504d7925338
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-04 10:13:38 -0300

    feat: adding LoginViewController.

--- MeusGastos/Source/ViewControllers/Login/LoginViewController.swift
+++ MeusGastos/Source/ViewControllers/Login/LoginViewController.swift
@@ -9,9 +9,20 @@ import Foundation
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    lazy var loginView: LoginView = {
+        let view = LoginView()
+        view.backgroundColor = .black
+        
+        return view
+    }()
+    
+    override func loadView() {
+        self.view = loginView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .purple
     }
 }
