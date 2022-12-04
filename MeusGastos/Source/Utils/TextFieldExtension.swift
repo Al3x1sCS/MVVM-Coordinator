b9a05c19736a93597c02c1ee58ae06d3910173f8
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-04 14:45:13 -0300

    feat: adding textField custom placeholder extension.

--- MeusGastos/Source/Utils/TextFieldExtension.swift
+++ MeusGastos/Source/Utils/TextFieldExtension.swift
@@ -4,5 +4,29 @@
 //
 //  Created by user on 04/12/22.
 //
+import UIKit
 
-import Foundation
+extension UITextField {
+    
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+    
+    func setPlaceHolderColor(){
+        self.attributedPlaceholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedString.Key.foregroundColor : UIColor.temaGray()])
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+      textField.resignFirstResponder()
+      return true
+    }
+    
+}
