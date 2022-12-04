17fb7ca41e0955f10a1462f030fdd27b3b16802a
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-04 14:46:01 -0300

    feat: adding theme.

--- MeusGastos/Source/Utils/Theme.swift
+++ MeusGastos/Source/Utils/Theme.swift
@@ -5,4 +5,28 @@
 //  Created by user on 04/12/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static func variationGreen() -> UIColor {
+        return UIColor.rgb(red: 126, green: 211, blue: 33)
+    }
+    
+    static func variationRed() -> UIColor {
+        return UIColor.rgb(red: 208, green: 2, blue: 27)
+    }
+    
+    static func temaGray() -> UIColor {
+        return UIColor.rgb(red: 151, green: 151, blue: 151)
+    }
+    
+    static func buttons() -> UIColor {
+        return UIColor.rgb(red: 60, green: 141, blue: 188)
+    }
+    
+}
