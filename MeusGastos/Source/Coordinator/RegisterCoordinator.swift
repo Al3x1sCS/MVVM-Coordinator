239cc902d751d055ccc4002bd7fbd90026fe7a92
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-05 17:05:18 -0300

    feat: Returns LoginView after pressing the button.

--- MeusGastos/Source/Coordinator/RegisterCoordinator.swift
+++ MeusGastos/Source/Coordinator/RegisterCoordinator.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class RegisterCoordinator: Coordinator {
+    let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewController = RegisterViewController()
+        
+        viewController.onLoginTap = {
+            self.showLogin()
+        }
+        
+        self.navigationController.pushViewController(viewController, animated: false)
+    }
+    
+    private func showLogin() {
+        self.navigationController.popToRootViewController(animated: false)
+    }
+    
+}
