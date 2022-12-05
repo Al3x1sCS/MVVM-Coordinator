28394e5a68a546ca92ac0613c5f3d8ddd4830858
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-05 11:16:39 -0300

    feat: organizing project.

---
+++ MeusGastos/Source/Infrastructure/Components/ButtonDefault.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonDefault.swift
+//  MeusGastos
+//
+//  Created by user on 05/12/22.
+//
+
+import Foundation
---
+++ MeusGastos/Source/Infrastructure/Components/TextFieldDefault.swift
@@ -0,0 +1,8 @@
+//
+//  TextFieldDefault.swift
+//  MeusGastos
+//
+//  Created by user on 05/12/22.
+//
+
+import Foundation
