2100eacd30a209dc9e32f0caffd593f815171669
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-05 17:11:59 -0300

    feat: create RegisterViewController.

--- MeusGastos/Source/ViewControllers/Register/RegisterViewController.swift
+++ MeusGastos/Source/ViewControllers/Register/RegisterViewController.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+
+class RegisterViewController: ViewControllerDefault {
+    //MARK: Clousures
+    var onLoginTap: (() -> Void)?
+    
+    //MARK: Visual Elements
+    lazy var registerView: RegisterView = {
+        let view = MeusGastos.RegisterView()
+        view.backgroundColor = .black
+        
+        view.onLoginTap = {
+            self.onLoginTap?()
+        }
+        
+        return view
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        view = registerView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+}
